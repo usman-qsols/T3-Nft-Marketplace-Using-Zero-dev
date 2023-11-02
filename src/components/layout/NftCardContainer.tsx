3682fb8454491873474f8ad03d1ff85b7ecc682a
usman-qsols <usmanrahim.qsols@gmail.com>
2023-11-02 17:58:41 +0500

    login issue resolved

--- src/components/layout/NftCardContainer.tsx
+++ src/components/layout/NftCardContainer.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import NftCard from "./NftCard";
-import auction1 from "../../utilities/auction-1.jpg";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+
 import { ZeroDevWeb3Auth } from "@zerodev/web3auth";
 import { useAccount } from "wagmi";
 import { api } from "~/utils/api";
@@ -13,18 +11,6 @@ const NftCardContainer = () => {
   const { data } = api.nft.all.useQuery();
   console.log("data", data);
 
-  useEffect(() => {
-    if (isConnected) {
-      const zeroDevWeb3Auth = ZeroDevWeb3Auth.getInstance([
-        process.env.REACT_APP_ZERODEV_PROJECT_ID ??
-          "b5486fa4-e3d9-450b-8428-646e757c10f6",
-      ]);
-      zeroDevWeb3Auth.getUserInfo().then(console.log);
-    }
-  }, [isConnected]);
-
-  if (!isConnected) {
-  }
   return (
     <section className="section explore" id="explore">
       <div className="container">
@@ -32,15 +18,6 @@ const NftCardContainer = () => {
 
         <div className="title-wrapper">
           <h2 className="h2 section-title">Explore</h2>
-
-          {/* <a href="#" className="btn-link">
-            <span>Explore All</span> */}
-
-          {/* <ion-icon
-              name="arrow-forward-outline"
-              aria-hidden="true"
-            ></ion-icon> */}
-          {/* </a> */}
         </div>
 
         <ul className="grid-list">
