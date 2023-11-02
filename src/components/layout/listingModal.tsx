0b98cd35183fe316c14f7463529e6a9ce89c8d0e
usman-qsols <usmanrahim.qsols@gmail.com>
2023-11-02 12:58:58 +0500

    ts disabled

--- src/components/layout/listingModal.tsx
+++ src/components/layout/listingModal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Loader from "../../utilities/loader.gif";
 import Image from "next/image";
 
+// eslint-disable-next-line
 const LoadingModal = (props: any) => {
   return (
     <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 py-10">
@@ -13,6 +14,7 @@ const LoadingModal = (props: any) => {
               alt="image"
               className="h-[80px] w-[80px] bg-red-500"
             ></Image>
+            {/* eslint-disable-next-line */}
             <h2 className="font-bold">{props.h2}</h2>
           </div>
         </div>
